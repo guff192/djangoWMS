5d1c58f70f4a70a30a42cfb7448574964aa19c8e
Dima <dchausov@inbox.ru>
2021-05-15 15:02:40 +0300

    initial comment - модели

--- wms/models.py
+++ wms/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class GoodTypes(models.Model):
+    barcode = models.IntegerField
+    article = models.CharField
+    weight = models.IntegerField
+    price = models.IntegerField
+    fragile = models.BooleanField
+
+
+class Cell(models.Model):
+    row = models.CharField
+    place = models.CharField
+
+
+class StoragePlaces(models.Model):
+    cell_id = models.ForeignKey(Cell)
+    type = models.CharField
+    length = models.IntegerField
+    width = models.IntegerField
+    height = models.IntegerField
+    max_weight = models.IntegerField
+
+
+class Bills(models.Model):
+    number = models.IntegerField
+    operation_type = models.IntegerField
+    date = models.DateField
+
+
+class Goods(models.Model):
+    barcode = models.ForeignKey(GoodTypes)
+    manufactured = models.DateField
+    best_before = models.DateField
+    BillsGoods = models.ManyToManyField(Bills)
+    PlacesGoods = models.ManyToManyField(StoragePlaces)
+
